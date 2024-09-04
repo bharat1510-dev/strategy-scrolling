afa9002cc2f871e73d7abed0f999a6ebaa376418
bharat1510-dev <bs365825@gmail.com>
2024-09-04 19:06:41 +0530

    Update Newsitem.js

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -4,7 +4,6 @@ export default function Newsitem(props) {
   return (
     <div>
      <div className="card" style={{width:"12rem"}}>
-  <img src={props.image} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{props.title}</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
